71825f31a5fdd3c5b5d9ee9fca0d6709e3f6a002
jeevana246 <69810945+jeevana246@users.noreply.github.com>
2021-12-18 14:29:39 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,21 +1,25 @@
-from django.urls.conf import path
-from compeapp import views
+"""compepro URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
+from compeapp import views
+from django.conf.urls import include
 
+import compeapp
 urlpatterns = [
-    path('',views.index,name="index"),
-    path('ureg',views.ureg,name="ureg"),
-    path('adminlogin',views.adminlogin,name="adminlogin"),
-    path('adminregister',views.adminregister,name="adminregister"),
-    path('scholarlogin',views.scholarlogin,name="scholarlogin"),
-    path('scholarregister',views.scholarregister,name="scholarregister"),
-    path('admin_dashboard',views.admin_dashboard,name="admin_dashboard"),
-    path('hackathons',views.hackathons,name="hackathons"),
-    path('competitions',views.competitions,name="competitions"),
-    path('events',views.events,name="events"),
-    path('adminlogout',views.adminlogout,name="adminlogout"),
+    path('admin/', admin.site.urls),
+    path('',include('compeapp.urls')),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
