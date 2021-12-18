9e22f9cee52f7d8049da0bebe652a5cf18820863
jeevana246 <69810945+jeevana246@users.noreply.github.com>
2021-12-18 14:25:20 +0530

    Delete admin.py

--- admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from compeapp.models import administrator, hackathon, scholar
-admin.site.register(administrator)
-admin.site.register(scholar)
-admin.site.register(hackathon)
